1794b56cce4026d9554d0320e6dc69ce9f3dca71
Mikhail Khvashchevskiy <khvashchevskiy.mikhail@gmail.com>
2022-07-28 13:01:48 +0300

    Final commit

---
+++ src/main/resources/db/migration/V1__Initial_script.sql
@@ -0,0 +1,31 @@
+create table company(
+	ID serial primary key not null,
+	indeed_name varchar(256),
+	yahoo_name varchar(256)
+);
+
+insert into company(indeed_name, yahoo_name) values('Ford', 'Ford');
+insert into company(indeed_name, yahoo_name) values('Honda', 'HMC');
+insert into company(indeed_name, yahoo_name) values('Dow', 'DOW');
+insert into company(indeed_name, yahoo_name) values('Sasol', 'SSL');
+insert into company(indeed_name, yahoo_name) values('Anglogold-Ashanti', 'AU');
+insert into company(indeed_name, yahoo_name) values('Alphabet', 'GOOG');
+insert into company(indeed_name, yahoo_name) values('Comcast', 'CMCSA');
+insert into company(indeed_name, yahoo_name) values('Meta-dd1502f2', 'META');
+insert into company(indeed_name, yahoo_name) values('Twitter', 'TWTR');
+insert into company(indeed_name, yahoo_name) values('Vodafone', 'VOD');
+insert into company(indeed_name, yahoo_name) values('Tesla', 'TSLA');
+insert into company(indeed_name, yahoo_name) values('Amazon.com', 'AMZN');
+insert into company(indeed_name, yahoo_name) values('McDonald%27s', 'MCD');
+insert into company(indeed_name, yahoo_name) values('Nike', 'NKE');
+insert into company(indeed_name, yahoo_name) values('Starbucks', 'SBUX');
+insert into company(indeed_name, yahoo_name) values('Booking.com', 'BKNG');
+insert into company(indeed_name, yahoo_name) values('Airbnb', 'ABNB');
+insert into company(indeed_name, yahoo_name) values('Walmart', 'WMT');
+insert into company(indeed_name, yahoo_name) values('ProcterGamble', 'PG');
+insert into company(indeed_name, yahoo_name) values('Coca-Cola', 'KO');
+insert into company(indeed_name, yahoo_name) values('DollarGeneral', 'DG');
+insert into company(indeed_name, yahoo_name) values('Colgate--palmolive', 'CL');
+insert into company(indeed_name, yahoo_name) values('Exxonmobil', 'XOM');
+insert into company(indeed_name, yahoo_name) values('Shell', 'SHEL');
+insert into company(indeed_name, yahoo_name) values('Wells-Fargo', 'WFC');
